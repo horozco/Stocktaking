64e8d39a2acdc68b902f3a6685f8d20b853e70c2
Usuario <hernan@ubuntu.(none)>
2013-02-12 17:53:36 -0500

    Finish all CRUD for Elements and Inventories.

--- app/controllers/elements_controller.rb
+++ app/controllers/elements_controller.rb
@@ -1,50 +1,43 @@
 class ElementsController < ApplicationController
   def create
-	  	@inventory = Inventory.find(params[:inventory_id])
-	  	@element = @inventory.elements.build(params[:element])
-	  	if @element.save
-		  respond_to do |format|
-	        format.html {redirect_to inventory_path(@inventory), :notice => "Your element was created :)"}
-	        format.js
-	      end  			
-	  	else
-	  	  respond_to do |format|
-		    format.html {redirect_to inventory_path(@inventory), :alert => "Your element couldn't be submitted. :("}
-		    format.js
-	      end
-	  	end
+  	@inventory = Inventory.find(params[:inventory_id])
+  	@element = @inventory.elements.build(params[:element])
+  	if @element.save
+	    flash[:notice] = "Se ha creado el elemento."
+	    redirect_to inventory_path(@inventory)		
+  	else
+	    flash[:alert] = "El elemento no se pudo crear."
+	    redirect_to inventory_path(@inventory)
+  	end
   end
 
   def destroy
-  		@inventory = Inventory.find(params[:inventory_id])
-	  	@element = Element.find(params[:id])	
-	  	if @element.destroy
-		  respond_to do |format|
-	        format.html {redirect_to inventory_path(@inventory), :notice => "Your element was destroy susscefully :)"}
-	        format.js
-	      end  			
-	  	else
-	  	  respond_to do |format|
-		    format.html {redirect_to inventory_path(@inventory), :alert => "Your element couldn't be destroy. :("}
-		    format.js
-	      end
-	  	end
-  end
-
-  def edit
-  		@inventory = Inventory.find(params[:inventory_id])
-	  	@element = Element.find(params[:id])
+		@inventory = Inventory.find(params[:inventory_id])
+  	@element = Element.find(params[:id])	
+  	if @element.destroy
+      flash[:notice] = "El elemento ha sido destruido."
+      redirect_to inventory_path(@inventory)
+  	else
+	    flash[:alert] = "No se pudo destruir el elemento."
+	    redirect_to inventory_path(@inventory)
+  	end
   end
 
   def update
-  	  	@inventory = Inventory.find(params[:inventory_id])
-	  	@element = Element.find(params[:id])
-	  	if @element.update_attributes(params[:element])
-      		redirect_to inventory_path(inventory), notice: "Element was updated" 
-    	else
-      		flash[:alert] = "Element wasn't updated :("
-      		render action: "edit"
-    	end		
+	  @inventory = Inventory.find(params[:inventory_id])
+  	@element = Element.find(params[:id])
+  	if @element.update_attributes(params[:element])
+    	flash[:notice] = "El elemento se ha actualizado."
+    	redirect_to inventory_path(@inventory)
+  	else
+  		flash[:alert] = "El elemento no pudo ser actualizado."
+  		redirect_to inventory_path(@inventory)
+  	end		
+  end
+
+  def show
+  	@inventory = Inventory.find(params[:inventory_id])
+  	@element = Element.find(params[:id])
   end
   
 end
--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,6 +1,6 @@
 class Inventory < ActiveRecord::Base
   attr_accessible :details, :site
   validates_presence_of :details, :site
-  belongs_to :user, dependent: :destroy
-  has_many :elements
+  belongs_to :user
+  has_many :elements, dependent: :destroy
 end
