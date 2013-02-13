fdcd8af21372f41eed3755ce56688ee13af79453
Hernan Orozco <horozco15@gmail.com>
2013-02-13 09:37:08 -0500

    Changed language.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
-    inventories_path
+    if resource.class != AdminUser
+      inventories_path
+    else
+      admin_root_path
+    end
   end
 end
