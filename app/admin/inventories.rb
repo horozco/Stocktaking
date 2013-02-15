36e76c38ee07d2f1846fe254dd0ef8b8838ed209
Hernan Orozco <horozco15@gmail.com>
2013-02-15 18:09:07 -0500

    Changed inventories admin view.

--- app/admin/inventories.rb
+++ app/admin/inventories.rb
@@ -1,3 +1,22 @@
+# encoding: utf-8
 ActiveAdmin.register Inventory do
-  
+  index :title => "Inventarios" do
+    column "Id", :id
+    column "Lugar", :site
+    column "Detalles", :details
+    column "Fecha de creación", :created_at
+    column "Fecha de actualización", :updated_at
+    default_actions
+  end
+
+  show do
+    attributes_table do
+      row :id
+      row :site
+      row :details
+      row :created_at
+      row :updated_at
+    end
+    active_admin_comments
+  end
 end
